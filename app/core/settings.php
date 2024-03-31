df22741403716b356b6595c5b28afc9e98f59528
Ruben Rodriguez <rubeneduardoforerorodriguez@gmail.com>
2024-03-30 22:36:16 -0500

    sistema_escolar_1.0

--- app/core/settings.php
+++ app/core/settings.php
@@ -16,8 +16,8 @@ define('SITE_LANG'   , $this->lng);
 // Versión de la aplicación
 define('BEE_NAME'    , $this->framework); // Viene desde Bee.php
 define('BEE_VERSION' , $this->version);   // Viene desde Bee.php
-define('SITE_NAME'   , 'EmpresaCool');    // Nombre del sitio
-define('SITE_VERSION', '1.0.0');          // Versión del sitio
+define('SITE_NAME'   , 'Aprende');    // Nombre del sitio
+define('SITE_VERSION', '1.0');          // Versión del sitio
 
 // Ruta base de nuestro proyecto
 // Esta constante ahora es configurada desde el archivo settings.php
